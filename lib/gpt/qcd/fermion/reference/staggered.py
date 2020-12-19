9cd1e67057c94ba8ec314ba6141fd7aa762b9149
Tilo Wettig <tilo.wettig@ur.de>
2020-12-19 23:55:06 +0100

    new staggered correct and tested

--- lib/gpt/qcd/fermion/reference/staggered.py
+++ lib/gpt/qcd/fermion/reference/staggered.py
@@ -26,12 +26,12 @@ from itertools import permutations
 
 class staggered(shift, matrix_operator):
     '''
-    M_xy = 1/2 sum_mu eta_mu(x) [ U_mu(x) delta_{x+mu,y} - U_mu^dagger(x-mu) delta_{x-mu,y} ] + m delta_xy
+    D_xy = 1/2 sum_mu eta_mu(x) [ U_mu(x) delta_{x+mu,y} - U_mu^dagger(x-mu) delta_{x-mu,y} ] + m delta_xy
     * gauge field could also be SU(2) or adjoint (see otype)
-    * nohop: if nonzero, turns off "hopping" term (for checks)
+    * nohop: if present, turns off "hopping" term (for checks)
     * mu5: coefficient of chiral chemical potential, see Eqs. (2.2) and (2.3) in
       https://link.springer.com/content/pdf/10.1007/JHEP06(2015)094.pdf
-    * theta5: if nonzero, chiral U(1) gauge field is present, passed in the U list after the SU field
+    * theta: if present, chiral U(1) gauge field is present, passed in the U list after the SU field
     '''
     @params_convention()
     def __init__(self, U, params):
@@ -44,16 +44,18 @@ class staggered(shift, matrix_operator):
         else:
             otype = g.ot_vector_color(Nc)
         grid = U[0].grid
-        self.mass  = params["mass"]
-        self.nohop = params["nohop"]
-        self.mu5   = params["mu5"]
-        self.theta = params["theta"]
-        
+
+        # sanity check for chiral U(1) gauge field
+        if "theta" in params:
+            assert "mu5" not in params, "cannot have both mu5 and theta"
+            assert len(U) == 8, "chiral U(1) field missing?"
+        self.params = params
+            
         self.Mooee = g.matrix_operator(
-            lambda dst, src: self._Mdiag(dst, src), otype=otype, grid=grid
+            lambda dst, src: self._Mooee(dst, src), otype=otype, grid=grid
         )
         self.Meooe = g.matrix_operator(
-            lambda dst, src: self._Mshift(dst, src), otype=otype, grid=grid
+            lambda dst, src: self._Meooe(dst, src), otype=otype, grid=grid
         )
         matrix_operator.__init__(
             self, lambda dst, src: self._M(dst, src), otype=otype, grid=grid
@@ -89,41 +91,47 @@ class staggered(shift, matrix_operator):
         for y in range(0,grid.fdimensions[1],2):
             self.s[:, y, :, :] = 1.0
             self.s[:, y+1, :, :] = -1.0
-            # self.s[:, :, :, y] = 1.0
-            # self.s[:, :, :, y+1] = -1.0
         # use stride > 1 once it is implemented:
         # self.s[1][:, 0::2, :, :] = 1.0
         # self.s[1][:, 1::2, :, :] = -1.0
 
-        # eps(x) = (-1)^{x1+x2+x3+x4}
-        # simplify this once even-odd structure is implemented
-        # core/grid.py: redblack
-        # core/lattice.py: checkerboard
-        # core/checkerboard.py
-        # core/coordinates.py
+        # role of gamma_5 is played by eps(x) = (-1)^{x1+x2+x3+x4}
+        # quick temporary hack 
+        # won't be needed anymore once even-odd structure is implemented
+        # see grid.py, lattice.py, checkerboard.py, coordinates.py
         self.eps = g.complex(grid)
-        
+        for x in range(0,grid.fdimensions[0]):
+            for y in range(0,grid.fdimensions[1]):
+                for z in range(0,grid.fdimensions[2]):
+                    for t in range(0,grid.fdimensions[3]):
+                        self.eps[x, y, z, t] = 1 - 2 * ( (x + y + z + t) % 2)
 
     def _Mooee(self, dst, src):
         assert dst != src
-        if (self.mass != 0):
-            dst @= self.mass * src
+#        if (self.mass != 0):
+        if "mass" in self.params:
+            dst @= self.params["mass"] * src
         else:
             dst[:] = 0
 
     def _Meooe(self, dst, src):
         assert dst != src
         dst[:] = 0
-        if (self.hop != 0):
-            for mu in range(4):
-                src_plus = g.eval(self.forward[mu] * src)
-                src_minus = g.eval(self.backward[mu] * src)
-                dst += self.phases[mu] * ( src_plus - src_minus ) / 2.0 
-        if (self.mu5 != 0):
+        if "mu5" in self.params:
             for [i, j, k] in permutations([0, 1, 2]):
                 src_plus = g.eval(self.forward[i] * self.forward[j] * self.forward[k] * src)
                 src_minus = g.eval(self.backward[k] * self.backward[j] * self.backward[i] * src)
-                dst += self.s * ( src_plus + src_minus ) * self.mu5 / (-12.0)
+                dst += self.s * ( src_plus + src_minus ) * self.params["mu5"] / (-12.0)
+        if "nohop" in self.params:
+            return
+        for mu in range(4):
+            if "theta" not in self.params:
+                src_plus = g.eval(self.forward[mu] * src)
+                src_minus = g.eval(self.backward[mu] * src)
+            else:
+                src_plus = g.eval(self.forward[mu] * src)
+                src_minus = g.eval(self.backward[mu] * src)
+            dst += self.phases[mu] * ( src_plus - src_minus ) / 2.0 
 
     def _M(self, dst, src):
         assert dst != src
